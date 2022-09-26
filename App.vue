39cc28c1c9e5ed70916a3602758ee0a2509a4061
wzlUUs <111121537+wzlUUs@users.noreply.github.com>
2022-09-26 22:07:46 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+/*每个页面公共css */
+<style lang="scss">
+	* {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	@import "./uni_modules/vk-uview-ui/index.scss";
+</style>
